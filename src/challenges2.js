36309475d3a088328f3710c053f3b7a4b84f7f05
Thales Santos <85639311+thalesorm@users.noreply.github.com>
2022-12-17 18:28:25 -0300

    :sparkles: feat: função triangleCheck pronta

--- src/challenges2.js
+++ src/challenges2.js
@@ -3,16 +3,45 @@ function generatePhoneNumber(numbers) {
   if (numbers.length !== 11) {
     return 'Array com tamanho incorreto.';
   }
-  if (numbers.some((n) => n < 0 || n > 9 || numbers.filter((num) => num === n).length >= 3)) {
+  if (
+    numbers.some(
+      (n) => n < 0 || n > 9 || numbers.filter((num) => num === n).length >= 3,
+    )
+  ) {
     return 'não é possível gerar um número de telefone com esses valores';
   }
   const phone = numbers.join('');
-  return `(${phone.substring(0, 2)}) ${phone.substring(2, 7)}-${phone.substring(7)}`;
+  return `(${phone.substring(0, 2)}) ${phone.substring(2, 7)}-${phone.substring(
+    7,
+  )}`;
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+
+function SumCheck(lineA, lineB, lineC) {
+  if (lineA < lineB + lineC && lineB < lineA + lineC && lineC < lineA + lineB) {
+    return true;
+  }
+  return false;
+}
+
+function absolutCheck(lineA, lineB, lineC) {
+  if (
+    lineA > Math.abs(lineB - lineC)
+    && lineB > Math.abs(lineA - lineC)
+    && lineC > Math.abs(lineA - lineB)
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function triangleCheck(lineA, lineB, lineC) {
+  // Verificar se é possível formar um triângulo com os comprimentos das três linhas
+  return (
+    SumCheck(lineA, lineB, lineC)
+    && absolutCheck(lineA, lineB, lineC)
+  );
 }
 
 // Desafio 13
