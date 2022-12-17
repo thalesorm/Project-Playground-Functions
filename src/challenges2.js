5c33438f636be73eac19aad34f8bf9f8c0623b97
Thales Santos <85639311+thalesorm@users.noreply.github.com>
2022-12-17 16:57:09 -0300

    :sparkles: feat: função generatePhoneNumber

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,13 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numbers) {
+  if (numbers.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  if (numbers.some((n) => n < 0 || n > 9 || numbers.filter((num) => num === n).length >= 3)) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  const phone = numbers.join('');
+  return `(${phone.substring(0, 2)}) ${phone.substring(2, 7)}-${phone.substring(7)}`;
 }
 
 // Desafio 12
