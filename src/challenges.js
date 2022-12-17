4b08101bae58f532c42877514fad1fc5e3bd6541
Thales Santos <85639311+thalesorm@users.noreply.github.com>
2022-12-17 12:01:20 -0300

    :sparkles: feat: função cat and mouse

--- src/challenges.js
+++ src/challenges.js
@@ -50,8 +50,19 @@ function highestCount(array) {
 }
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
+  let result = '';
+  let dist1 = Math.abs(cat1 - mouse);
+  let dist2 = Math.abs(cat2 - mouse);
+  if (dist1 === dist2) {
+    result = 'os gatos trombam e o rato foge';
+  } else if (dist1 < dist2) {
+    result = 'cat1';
+  } else {
+    result = 'cat2';
+  }
+  return result;
 }
 
 // Desafio 8
